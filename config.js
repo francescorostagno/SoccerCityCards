1f409181be8f274bfcc5c6ea4bd5d1edc98f8110
francescorostagno <rostagno.francesc@gmail.com>
2022-11-07 17:08:28 +0100

    db

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     'mysql': {
         'host': '127.0.0.1',
-        'user': 'root',
-        'password': 'root',
-        'database': 'SoccerCityCards',
-        'port': 8889
+        'user': 'b7c11c2f5c5ed8',
+        'password': 'cf5cd4da2f6d62b',
+        'database': 'heroku_dd464d21811495b',
+        //'port': 8889
     },
     'google':{
         'clientId': '277768178442-91eeosne0uhrfcht63ia2iht6ntkpi7m.apps.googleusercontent.com',
