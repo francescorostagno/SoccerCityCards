49f298bb2255a29bf0dd1dae3fa734a49879e45b
francescorostagno <rostagno.francesc@gmail.com>
2022-11-07 17:10:55 +0100

    db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'mysql': {
-        'host': '127.0.0.1',
+        'host': 'eu-cdbr-west-03.cleardb.net',
         'user': 'b7c11c2f5c5ed8',
         'password': 'cf5cd4da2f6d62b',
         'database': 'heroku_dd464d21811495b',
