40c068bd3c8b946926da377ca4c3923741a225bd
francescorostagno <rostagno.francesc@gmail.com>
2022-11-09 16:49:20 +0100

    db

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
         'host': 'sql7.freemysqlhosting.net',//'eu-cdbr-west-03.cleardb.net',
         'user': 'sql7552436',//'b7c11c2f5c5ed8',
         'password': '9jQZZMyhYF',//'cf5cd4da2f6d62b',
-        'database': 'sql7552436'
+        'database': 'sql7552436',
+        'port': 3306
     },
     'google':{
         'clientId': '277768178442-91eeosne0uhrfcht63ia2iht6ntkpi7m.apps.googleusercontent.com',
