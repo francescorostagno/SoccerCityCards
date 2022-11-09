7ee3b6a1da335684eb7317cfdd3c68ee81e5217b
francescorostagno <rostagno.francesc@gmail.com>
2022-11-09 16:42:32 +0100

    break page

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'mysql': {
         'host': 'sql7.freemysqlhosting.net',//'eu-cdbr-west-03.cleardb.net',
         'user': 'sql7552436',//'b7c11c2f5c5ed8',
-        'password': 'sql7552436',//'cf5cd4da2f6d62b',
-        'database': '9jQZZMyhYF'
+        'password': '9jQZZMyhYF',//'cf5cd4da2f6d62b',
+        'database': 'sql7552436'
     },
     'google':{
         'clientId': '277768178442-91eeosne0uhrfcht63ia2iht6ntkpi7m.apps.googleusercontent.com',
